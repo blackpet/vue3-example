e1e97f5cecd23aa54b3b15036f42d3dce376825e
blcakpet <blackept@gmail.com>
2021-11-08 02:08:00 +0900

    Component: Modal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import Backdrop from './components/Backdrop.vue';
+import Modal from './components/Modal.vue';
 import {ref} from 'vue';
 
+const backdrop1 = ref(null)
+const backdrop2 = ref(null)
 const modal1 = ref(null)
+const modal2 = ref(null)
+
+function logClose() {
+  console.log('modal is closed')
+}
 
 </script>
 
 <template>
   <div class="container mx-auto">
     <div class="p-8 bg-green-200 flex gap-8 justify-center">
+      <button @click="backdrop1.open">open backdrop</button>
+      <button @click="backdrop2.open">open backdrop with body</button>
       <button @click="modal1.open">open modal</button>
-      <button @click="modal1.close">close modal</button>
+      <button @click="modal2.open">open modal with body</button>
     </div>
 
     <div>
@@ -26,7 +36,21 @@ const modal1 = ref(null)
     </div>
   </div>
 
-  <Backdrop ref="modal1" />
+  <Backdrop ref="backdrop1" />
+  <Backdrop ref="backdrop2">
+    <div class="bg-red-500 text-white relative rounded-lg shadow-xl px-8 py-4">
+      hi backdrop
+    </div>
+  </Backdrop>
+
+  <Modal ref="modal1"
+         title="modal example1"
+         classes="w-[500px]"
+         @close="logClose" />
+  <Modal ref="modal2" title="modal example2"
+         @close="logClose">
+    body~ buddy~
+  </Modal>
 </template>
 
 <style>
